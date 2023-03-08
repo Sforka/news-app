ecddfd8e6434d54dcae25d0e00022355a7a9042e
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-03-08 13:26:28 +0100

    superDDone

---
+++ src/js/setFavoritesInLocalStore.js
@@ -0,0 +1,36 @@
+//===добавляет избранное в локальное хранилище ==========
+export default function setFavoritesInLocalStor({
+  resultsArr,
+  clickedArticleId,
+  evt,
+}) {
+  const STORAGE_FAVORITES_KEY = 'favorites';
+  resultsArr.forEach(article => {
+    if (
+      article.id == clickedArticleId ||
+      article.slug_name == clickedArticleId ||
+      article._id == clickedArticleId
+    ) {
+      let savedData = localStorage.getItem(STORAGE_FAVORITES_KEY);
+
+      // проверка или есть уже обьект
+      savedData = savedData ? JSON.parse(savedData) : {};
+
+      if (savedData[clickedArticleId]) {
+        delete savedData[`${clickedArticleId}`];
+
+        if ((evt.target.textContent.contains = 'Remove from favorites')) {
+          evt.target.textContent = 'Add to favorites';
+        }
+
+        localStorage.setItem(STORAGE_FAVORITES_KEY, JSON.stringify(savedData));
+        return;
+      } else {
+        savedData[clickedArticleId] = article;
+
+        localStorage.setItem(STORAGE_FAVORITES_KEY, JSON.stringify(savedData));
+      }
+    }
+  });
+}
+//== добавляет избранное в локальное хранилище. конец ==========

4b9dfe46fca6a0647eb1d03e3eb6f3444731e92a
Sforka <warlock20115@gmail.com>
2023-03-10 16:54:04 +0200

    1
    1

--- src/read.js
+++ src/read.js
@@ -221,24 +221,6 @@ themeSwitcherEl.addEventListener('change', themeSwitcher.onThemeToggle);
 themeSwitcher.renderTheme();
 //============= перемикач теми кінець ============
 
-// начало. переформатирование даты
-function publishedDateFormatter(date) {
-  return formatDate(new Date(date));
-}
-
-function formatDate(date) {
-  return [
-    padTo2Digits(date.getDate()),
-    padTo2Digits(date.getMonth() + 1),
-    date.getFullYear(),
-  ].join('/');
-}
-
-function padTo2Digits(num) {
-  return num.toString().padStart(2, '0');
-}
-// конецю переформатирование даты
-
 // Начало. Проверка на клик по Добавить в избранное
 body.addEventListener('click', onAddToFavoritesClick);
 body.addEventListener('click', onAddToReadClick);
